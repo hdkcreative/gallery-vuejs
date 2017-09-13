8137f0c35ec926aa80cb8a533992210a92f8ed69
Hely Rojas <miguelrojasvargas@gmail.com>
2017-09-13 13:50:32 -0300

    Create gallery-vue.js

---
+++ gallery-vue.js
@@ -0,0 +1,29 @@
+
+    Vue.use(vScroll) 
+
+ new Vue({
+   el:    '#gallery',
+
+  data: {
+         isActive: '',
+         elementsQuantity : 8,
+         x:-1
+  },
+  methods: {
+    addShowClass: function (event) {
+      targetId = event;
+        this.isActive=event.target.getAttribute('data-index');
+        console.log("isActive = "+ this.isActive)
+    },
+
+    removeShowClass: function () {
+      this.isActive= ''
+    },
+
+   
+
+
+
+  }
+});
+
